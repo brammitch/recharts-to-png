95fc4a24d183bd3cc996af527fb4273b50fd9a15
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2020-07-18 17:58:01 -0400

    Organize imports.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-import { AreaChart, PieChart, RadarChart, ScatterChart, BarChart, ComposedChart, LineChart, RadialBarChart, Treemap } from 'recharts';
-export declare type RechartChart = AreaChart | BarChart | ComposedChart | LineChart | PieChart | RadarChart | RadialBarChart | ScatterChart | Treemap;
-export declare function getPngData(chart: RechartChart, height: number, width: number): Promise<string>;
+import { AreaChart, BarChart, ComposedChart, LineChart, PieChart, RadarChart, RadialBarChart, ScatterChart, Treemap } from 'recharts';
+export declare type RechartsChart = AreaChart | BarChart | ComposedChart | LineChart | PieChart | RadarChart | RadialBarChart | ScatterChart | Treemap;
+export declare function getPngData(chart: RechartsChart, height: number, width: number): Promise<string>;
