4c8796ca80a9135dc52c0eed2fbe4c9d7c9bbe56
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2020-12-17 13:13:13 -0500

    fix(lib): :label: update types to match current version

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,16 @@
+/// <reference types="react" />
+/// <reference types="html2canvas" />
 import { AreaChart, BarChart, ComposedChart, LineChart, PieChart, RadarChart, RadialBarChart, ScatterChart, Treemap } from 'recharts';
 export declare type RechartsChart = AreaChart | BarChart | ComposedChart | LineChart | PieChart | RadarChart | RadialBarChart | ScatterChart | Treemap;
-export declare function getPngData(chart: RechartsChart, height: number, width: number): Promise<string>;
+/**
+ * @deprecated Not compatible with React 17. Use the useRechartToPng hook instead.
+ * Returns a PNG URL string
+ * @param instance - The Rechart component to generate the PNG for
+ * @param options - Html2Canvas formatting options
+ */
+export declare function getPngData(instance: Element | React.Component | RechartsChart, options?: Html2CanvasOptions): Promise<string>;
+/**
+ * Returns a PNG URL string
+ * @param options - Html2Canvas formatting options
+ */
+export declare function useRechartToPng(options?: Html2CanvasOptions): [string, (node: unknown) => Promise<void>];
