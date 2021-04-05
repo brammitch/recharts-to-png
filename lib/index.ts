e332880a30f055111e609a0200fbaf5f95d23341
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2021-04-05 16:05:46 -0400

    refactor(lib): :arrow_up: Upgrade for compatibility with recharts v2
    Remove deprecated getPngData function
    
    #64

--- lib/index.ts
+++ lib/index.ts
@@ -1,48 +1,5 @@
 import html2canvas from 'html2canvas';
 import { useCallback, useState } from 'react';
-import ReactDOM from 'react-dom';
-import {
-  AreaChart,
-  BarChart,
-  ComposedChart,
-  LineChart,
-  PieChart,
-  RadarChart,
-  RadialBarChart,
-  ScatterChart,
-  Treemap,
-} from 'recharts';
-
-export type RechartsChart =
-  | AreaChart
-  | BarChart
-  | ComposedChart
-  | LineChart
-  | PieChart
-  | RadarChart
-  | RadialBarChart
-  | ScatterChart
-  | Treemap;
-
-/**
- * @deprecated Not compatible with React 17. Use the useRechartToPng hook instead.
- * Returns a PNG URL string
- * @param instance - The Rechart component to generate the PNG for
- * @param options - Html2Canvas formatting options
- */
-export async function getPngData(
-  instance: Element | React.Component | RechartsChart,
-  options?: Html2CanvasOptions
-): Promise<string> {
-  // eslint-disable-next-line react/no-find-dom-node
-  const element = ReactDOM.findDOMNode(instance) as HTMLElement;
-
-  const pngData = await html2canvas(element, options).then((canvas) =>
-    canvas.toDataURL('image/png', 1.0)
-  );
-
-  return pngData;
-}
 
 /**
  * Returns a PNG URL string
