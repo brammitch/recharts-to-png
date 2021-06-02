b406007fe3d4436f88ac9c26919c9952f8e55359
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2021-06-02 12:24:17 -0400

    chore(other): :memo: add demo project

---
+++ examples/demo/config-overrides.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+
+module.exports = function override(config) {
+  config.resolve.plugins = config.resolve.plugins.filter(
+    (plugin) => !(plugin instanceof ModuleScopePlugin)
+  );
+
+  return config;
+};
