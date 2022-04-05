e1ee05e6cd7d2f1e0a6506ca40ecbb6cfb3f8bcc
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2022-04-05 15:29:22 -0400

    build(deps): :arrow_up: use react 18

--- examples/demo/src/index.tsx
+++ examples/demo/src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if (container) createRoot(container).render(<App />);
