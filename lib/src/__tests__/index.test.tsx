91b897ae0d778cd763b79c1b9d29bddd076309bf
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2022-06-03 12:13:10 -0400

    test(lib): :white_check_mark: add tests for component; update jest config

--- lib/src/__tests__/index.test.tsx
+++ lib/src/__tests__/index.test.tsx
@@ -1,10 +1,22 @@
-import { renderHook } from '@testing-library/react';
-import { useCurrentPng } from '../index';
+import { render, renderHook, screen } from '@testing-library/react';
+import { CurrentPng, CurrentPngProps, useCurrentPng } from '../index';
 
 describe('useCurrentPng', () => {
   test('returns a tuple', async () => {
     const { result } = renderHook(() => useCurrentPng());
 
     expect(result.current.length).toBe(2);
+  });
+});
+
+function TestComponent(props: CurrentPngProps): JSX.Element {
+  return <div>{props.isLoading ? 'Loading' : 'Not Loading'}</div>;
+}
+
+describe('CurrentPng', () => {
+  test('does stuff', () => {
+    render(<CurrentPng>{(props) => <TestComponent {...props} />}</CurrentPng>);
+
+    expect(screen.getByText('Not Loading')).toBeDefined();
   });
 });
