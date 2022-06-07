0c7cb16f3dd96827c3484abb8a9a00b71b0cc80c
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2022-06-07 09:06:52 -0400

    test(lib): :white_check_mark: add test for CurrentPng

--- lib/src/__tests__/index.test.tsx
+++ lib/src/__tests__/index.test.tsx
@@ -1,5 +1,8 @@
-import { render, renderHook, screen } from '@testing-library/react';
+import { fireEvent, render, renderHook, screen } from '@testing-library/react';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
 import { CurrentPng, CurrentPngProps, useCurrentPng } from '../index';
+
+jest.setTimeout(10000);
 
 describe('useCurrentPng', () => {
   test('returns a tuple', async () => {
@@ -9,14 +12,37 @@ describe('useCurrentPng', () => {
   });
 });
 
-function TestComponent(props: CurrentPngProps): JSX.Element {
-  return <div>{props.isLoading ? 'Loading' : 'Not Loading'}</div>;
+function TestComponent(props: CurrentPngProps) {
+  const handleClick = async () => {
+    await props.getPng();
+  };
+
+  return (
+    <>
+      <LineChart width={200} height={200} data={[{ name: 'a', amt: 1 }]} ref={props.chartRef}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Line type="monotone" dataKey="amt" stroke="#82ca9d" />
+      </LineChart>
+      <button onClick={() => handleClick()}>Get Png</button>
+    </>
+  );
 }
 
 describe('CurrentPng', () => {
-  test('does stuff', () => {
-    render(<CurrentPng>{(props) => <TestComponent {...props} />}</CurrentPng>);
+  test('uses render props', async () => {
+    const getPng = jest.fn();
 
-    expect(screen.getByText('Not Loading')).toBeDefined();
+    render(
+      <CurrentPng>
+        {({ chartRef, isLoading }) => (
+          <TestComponent chartRef={chartRef} getPng={getPng} isLoading={isLoading} />
+        )}
+      </CurrentPng>
+    );
+
+    const button = await screen.findByText('Get Png');
+    fireEvent.click(button);
+    expect(getPng).toHaveBeenCalledTimes(1);
   });
 });
