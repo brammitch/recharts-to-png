52c1ce4457027020dfacd6e1a21d45c60e77dc25
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2023-10-20 09:18:39 -0400

    feat(lib): :sparkles: add useGenerateImage; update deps
    Closes #634

--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,6 +1,57 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import html2canvas, { Options } from 'html2canvas';
+import html2canvas, { Options as HTML2CanvasOptions } from 'html2canvas';
 import { Component, createRef, useCallback, useRef, useState } from 'react';
+
+export type UseGenerateImage<T extends HTMLElement = HTMLDivElement> = [
+  () => Promise<string | undefined>,
+  {
+    isLoading: boolean;
+    ref: React.MutableRefObject<T | null>;
+  }
+];
+
+export type UseGenerateImageArgs = {
+  options?: HTML2CanvasOptions;
+  quality?: number;
+  type?: string;
+};
+
+/**
+ * @param {{options: HTML2CanvasOptions, type: string, quality: number}} [args] Optional arguments.
+ * @param {HTML2CanvasOptions} [args.options] An html2canvas Options object.
+ * @param {number} [args.quality] Applies if the type is an image format that supports variable quality (such as "image/jpeg"), and is a number in the range 0.0 to 1.0 inclusive indicating the desired quality level for the resulting image.
+ * @param {string} [args.type] Controls the type of the image to be returned (e.g. PNG or JPEG). The default is "image/png"; that type is also used if the given type isn't supported.
+ *
+ * [Reference: html.spec.whatwg.org](https://html.spec.whatwg.org/multipage/canvas.html#dom-canvas-todataurl-dev)
+ */
+export function useGenerateImage<T extends HTMLElement = HTMLDivElement>(
+  args?: UseGenerateImageArgs
+): UseGenerateImage<T> {
+  const ref = useRef<T>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const generateImage = useCallback(async () => {
+    if (ref !== null && ref?.current) {
+      setIsLoading(true);
+
+      return await html2canvas(ref.current as HTMLElement, {
+        logging: false,
+        ...args?.options,
+      }).then((canvas) => {
+        setIsLoading(false);
+        return canvas.toDataURL(args?.type, args?.quality);
+      });
+    }
+  }, [args]);
+
+  return [
+    generateImage,
+    {
+      ref,
+      isLoading,
+    },
+  ];
+}
 
 export type UseCurrentPng = [
   () => Promise<string | undefined>,
@@ -13,7 +64,7 @@ export type UseCurrentPng = [
 /**
  * @param options - optional html2canvas Options object
  */
-export function useCurrentPng(options?: Partial<Options>): UseCurrentPng {
+export function useCurrentPng(options?: Partial<HTML2CanvasOptions>): UseCurrentPng {
   const ref = useRef<any>();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -42,7 +93,7 @@ export function useCurrentPng(options?: Partial<Options>): UseCurrentPng {
 
 export interface CurrentPngProps {
   chartRef: React.RefObject<any>;
-  getPng: (options?: Partial<Options>) => Promise<string | undefined>;
+  getPng: (options?: Partial<HTML2CanvasOptions>) => Promise<string | undefined>;
   isLoading: boolean;
 }
 
@@ -61,7 +112,7 @@ export class CurrentPng extends Component<Props, State> {
     isLoading: false,
   };
 
-  getPng = async (options?: Partial<Options>) => {
+  getPng = async (options?: Partial<HTML2CanvasOptions>) => {
     if (this.chartRef.current?.container) {
       this.setState({ isLoading: true });
 
